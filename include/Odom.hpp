47613736bb67fe8cc9cb8eac19d10741e7618e8c
Chen An Hong <cahchenanhong@gmail.com>
2021-12-15 16:41:38 +0800

    init working

---
+++ include/Odom.hpp
@@ -0,0 +1,9 @@
+#ifndef _8059_PP_ODOM_HPP_
+#define _8059_PP_ODOM_HPP_
+#include <algorithm>
+#include "Node.hpp"
+#define R_DIS 5.0
+#define S_DIS 6.5
+#define inPerDeg 0.024182854666401 //use baseOdom to tune?
+void Odometry(void * ignore);
+#endif
---
+++ src/Sensors.cpp
@@ -0,0 +1,29 @@
+#include "main.h"
+/** global variables */
+Node position;
+double encdR = 0, encdS = 0, bearing = 0, angle = halfPI;
+double measuredV = 0, measuredVL = 0, measuredVR = 0;
+// angle = mathematical angle, taken from x-axis, counter clockwise as positive
+void Sensors(void * ignore){
+  // port data from all sensors
+  Motor FL (FLPort);
+  Motor BL (BLPort);
+  Motor FR (FRPort);
+  Motor BR (BRPort);
+  Imu imu(imuPort);
+  ADIEncoder encoderR(encdR_port, encdR_port+1, false);
+  ADIEncoder encoderS(encdS_port, encdS_port+1, false);
+  bool calibrated = false;
+  int start = millis();
+  while(true){
+    encdR = encoderR.get_value()*inPerDeg;
+    encdS = -encoderS.get_value()*inPerDeg;
+    bearing = imu.is_calibrating()? 0 : imu.get_rotation()*toRad;
+    angle = boundRad(halfPI - bearing);
+    // measuredVL = (FL.get_actual_velocity() + BL.get_actual_velocity())/2 * RPMToInPerMs;
+    // measuredVR = (FR.get_actual_velocity() + BR.get_actual_velocity())/2 * RPMToInPerMs;;
+    measuredV = (measuredVL + measuredVR)/2;
+
+    delay(5);
+  }
+}
